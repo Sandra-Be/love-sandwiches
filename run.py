52bb0bfcee595c1adbf10d05da7a1529ef7e5e59
Sandra-Be <sandraberzk@gmail.com>
2021-08-09 21:42:18 +0000

    Grammar check

--- run.py
+++ run.py
@@ -44,7 +44,7 @@ def validate_data(values):
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
-        )
+            )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
